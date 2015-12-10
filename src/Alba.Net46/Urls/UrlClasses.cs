c884e8c860eac61b01442bc1f28c77b047d3075a
jeremydmiller <jeremydmiller@yahoo.com>
2015-12-10 14:13:40 -0600

    updated Baseline to get the conversions

--- src/Alba.Net46/Urls/UrlClasses.cs
+++ src/Alba.Net46/Urls/UrlClasses.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq.Expressions;
 using System.Reflection;
 using Alba.Routing;
-using Alba.Util;
+using Baseline;
 
 namespace Alba.Urls
 {
