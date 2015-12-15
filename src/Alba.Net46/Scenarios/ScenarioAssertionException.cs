81b6cb4cc831c23650ff16c453101a553c7de5b4
jeremydmiller <jeremydmiller@yahoo.com>
2015-12-15 10:24:18 -0600

    tested out ScenarioAssertionException

---
+++ src/Alba.Net46/Scenarios/ScenarioAssertionException.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using Baseline;
+
+namespace Alba.Scenarios
+{
+    [Serializable]
+    public class ScenarioAssertionException : Exception
+    {
+        private readonly IList<string> _messages = new List<string>();
+
+        public ScenarioAssertionException()
+        {
+        }
+
+        protected ScenarioAssertionException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+
+        public void Add(string message)
+        {
+            _messages.Add(message);
+        }
+
+        public void AssertValid()
+        {
+            if (_messages.Any())
+            {
+                throw this;
+            }
+        }
+
+        public IEnumerable<string> Messages => _messages;
+
+        public override string Message
+        {
+            get
+            {
+                var writer = new StringWriter();
+                _messages.Each(x => writer.WriteLine((string) x));
+
+                if (Body.IsNotEmpty())
+                {
+                    writer.WriteLine();
+                    writer.WriteLine();
+                    writer.WriteLine("Actual body text was:");
+                    writer.WriteLine();
+                    writer.WriteLine(Body);
+                }
+
+                return writer.ToString();
+            }
+        }
+
+        public string Body { get; set; }
+    }
+}
--- src/Alba.Net46/Scenarios/ScenarioClasses.cs
+++ src/Alba.Net46/Scenarios/ScenarioClasses.cs
@@ -4,7 +4,6 @@ using System.Collections.Specialized;
 using System.IO;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
@@ -248,59 +247,4 @@ namespace Alba.Scenarios
         }
 
     }
-
-
-    [Serializable]
-    public class ScenarioAssertionException : Exception
-    {
-        private readonly IList<string> _messages = new List<string>();
-
-        public ScenarioAssertionException()
-        {
-        }
-
-        protected ScenarioAssertionException(SerializationInfo info, StreamingContext context)
-            : base(info, context)
-        {
-        }
-
-        public void Add(string message)
-        {
-            _messages.Add(message);
-        }
-
-        public void AssertValid()
-        {
-            if (_messages.Any())
-            {
-                throw this;
-            }
-        }
-
-        public IEnumerable<string> Messages => _messages;
-
-        public override string Message
-        {
-            get
-            {
-                var writer = new StringWriter();
-                _messages.Each(x => writer.WriteLine(x));
-
-                if (Body.IsNotEmpty())
-                {
-                    writer.WriteLine();
-                    writer.WriteLine();
-                    writer.WriteLine("Actual body text was:");
-                    writer.WriteLine();
-                    writer.WriteLine(Body);
-                }
-
-                return writer.ToString();
-            }
-        }
-
-        public string Body { get; set; }
-    }
-
-
 }
