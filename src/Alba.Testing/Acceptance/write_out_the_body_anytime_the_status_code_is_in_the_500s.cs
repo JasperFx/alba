2d5e8231fd90c56fcee9c22f78bd015ea7467bc0
Jeremy D. Miller <jeremydmiller@yahoo.com>
2017-01-19 09:39:50 -0600

    just verifying that the response body is displayed if the status code >= 500. Closes GH-12

---
+++ src/Alba.Testing/Acceptance/write_out_the_body_anytime_the_status_code_is_in_the_500s.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Shouldly;
+using Xunit;
+
+namespace Alba.Testing.Acceptance
+{
+    public class write_out_the_body_anytime_the_status_code_is_in_the_500s : ScenarioContext
+    {
+        [Fact]
+        public async Task will_write_out_the_body_on_500()
+        {
+            host.Handlers["/one"] = c =>
+            {
+                c.Response.StatusCode = 500;
+                return c.Response.WriteAsync(new NotSupportedException().ToString());
+            };
+
+            var ex = await fails(_ =>
+            {
+                _.Get.Url("/one");
+                _.StatusCodeShouldBeOk();
+            });
+
+            ex.Message.ShouldContain("NotSupportedException");
+        }
+
+        [Fact]
+        public async Task will_write_out_the_body_on_501()
+        {
+            host.Handlers["/one"] = c =>
+            {
+                c.Response.StatusCode = 501;
+                return c.Response.WriteAsync(new NotSupportedException().ToString());
+            };
+
+            var ex = await fails(_ =>
+            {
+                _.Get.Url("/one");
+                _.StatusCodeShouldBeOk();
+            });
+
+            ex.Message.ShouldContain("NotSupportedException");
+        }
+
+        [Fact]
+        public async Task will_write_out_the_body_on_502()
+        {
+            host.Handlers["/one"] = c =>
+            {
+                c.Response.StatusCode = 501;
+                return c.Response.WriteAsync(new DivideByZeroException().ToString());
+            };
+
+            var ex = await fails(_ =>
+            {
+                _.Get.Url("/one");
+                _.StatusCodeShouldBeOk();
+            });
+
+            ex.Message.ShouldContain("DivideByZeroException");
+        }
+    }
+}
