92bce8fa65f64968b3a0c5dc7a8a836502ab7a0a
Jeremy D. Miller <jeremydmiller@yahoo.com>
2017-02-02 08:19:54 -0600

    implemented the ContentLength/ContentType props in StubHttpRequest

--- src/Alba/Stubs/StubHttpRequest.cs
+++ src/Alba/Stubs/StubHttpRequest.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.Extensions.Primitives;
 
 namespace Alba.Stubs
 {
@@ -31,8 +33,36 @@ namespace Alba.Stubs
         public override string Protocol { get; set; }
         public override IHeaderDictionary Headers { get; } = new HeaderDictionary();
         public override IRequestCookieCollection Cookies { get; set; } = new RequestCookieCollection();
-        public override long? ContentLength { get; set; }
-        public override string ContentType { get; set; }
+
+        public override long? ContentLength
+        {
+            get
+            {
+                if (!Headers.ContainsKey("content-length"))
+                {
+                    return null;
+                }
+
+                return long.Parse(Headers["content-length"].First());
+            }
+            set
+            {
+                Headers["content-length"] = new StringValues(value.ToString());
+            }
+        }
+
+        public override string ContentType
+        {
+            get
+            {
+                return Headers["content-type"].FirstOrDefault();
+            }
+            set
+            {
+                Headers["content-type"] = new StringValues(value);
+            }
+        }
+
         public override Stream Body { get; set; } = new MemoryStream();
         public override bool HasFormContentType { get; } = false; // TODO -- do something here.
         public override IFormCollection Form { get; set; }
