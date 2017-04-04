7ff204ab77df79531bb41f2a9e6f3ac999bef434
jeremydmiller <jeremydmiller@yahoo.com>
2017-04-04 10:47:43 -0500

    Docs on status code

--- documentation/documentation/statuscode.md
+++ documentation/documentation/statuscode.md
@@ -1,1 +1,9 @@
 <!--title: Http Status Codes-->
+
+You can declaratively check the status code with this syntax:
+
+<[sample:check-the-status-code]>
+
+Do note that by default, if you do not specify the expected status code, Alba assumes that
+the request should return 200 (OK) and will fail the scenario if a different status code is found. You
+can ignore that check with the `Scenario.IgnoreStatusCode()` method.
---
+++ src/Alba.Testing/Samples/StatusCodes.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+
+namespace Alba.Testing.Samples
+{
+    public class StatusCodes
+    {
+        // SAMPLE: check-the-status-code
+        public Task check_the_status(ISystemUnderTest system)
+        {
+            return system.Scenario(_ =>
+            {
+                // Shorthand for saying that the StatusCode should be 200
+                _.StatusCodeShouldBeOk();
+
+                // Or a specific status code
+                _.StatusCodeShouldBe(403);
+
+                // Ignore the status code altogether
+                _.IgnoreStatusCode();
+            });
+        }
+        // ENDSAMPLE
+    }
+}
