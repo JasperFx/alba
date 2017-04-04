b5d8947fb2caac6f094c955c8d2fc16c79c83abe
jeremydmiller <jeremydmiller@yahoo.com>
2017-04-04 10:05:11 -0500

    Docs on working with headers

--- documentation/documentation/headers.md
+++ documentation/documentation/headers.md
@@ -1,1 +1,27 @@
 <!--title: Working with Headers-->
+
+We really didn't worry much about HTTP niceties when I started web programming in the late 90's, but you
+can no longer get away with that in today's world. To more correctly use HTTP, Alba comes with some helpers
+to deal with HTTP header values.
+
+## Setting Request Headers
+
+To set request headers, you can directly write against the `HttpContext.Request.Headers` collection:
+
+<[sample:setting-request-headers]>
+
+There are also some specific helpers for very common [content negotiation-related](https://en.wikipedia.org/wiki/Content_negotiation) headers:
+
+<[sample:conneg-helpers]>
+
+**Do add extension methods off of the Alba `Scenario` class for common operations in your tests to remove
+some of the tedium.**
+
+## Asserting on Expected Response Headers
+
+Alba comes with some out of the box assertions to declaratively check expected header values:
+
+<[sample:asserting-on-header-values]>
+
+You do also have the ability to just interrogate the `HttpContext.Response` in your unit test methods for
+anything not covered in the helpers above. To add your own custom assertions, see <[linkto:documentation/assertions]>.
---
+++ src/Alba.Testing/Samples/Headers.cs
@@ -0,0 +1,60 @@
+﻿using System.Threading.Tasks;
+
+namespace Alba.Testing.Samples
+{
+    public class Headers
+    {
+        // SAMPLE: conneg-helpers
+        public Task conneg_helpers(ISystemUnderTest system)
+        {
+            return system.Scenario(_ =>
+            {
+                // Set the accepts header on the request
+                _.Get.Url("/").Accepts("text/plain");
+
+                // Specify the etag header value
+                _.Get.Url("/").Etag("12345");
+
+                // Set the content-type header on the request
+                _.Post.Url("/").ContentType("text/json");
+
+                // This is a superset of the code above that
+                // will set the content-type header as well
+                _.Post.Json(new InputModel()).ToUrl("/");
+            });
+        }
+        // ENDSAMPLE
+
+        // SAMPLE: setting-request-headers
+        public Task setting_request_headers(ISystemUnderTest system)
+        {
+            return system.Scenario(_ =>
+            {
+                _.Context.Request.Headers["foo"] = "bar";
+            });
+        }
+        // ENDSAMPLE
+
+
+        // SAMPLE: asserting-on-header-values
+        public Task asserting_on_header_values(ISystemUnderTest system)
+        {
+            return system.Scenario(_ =>
+            {
+                // Assert that there is one and only one value equal to "150"
+                _.Header("content-length").SingleValueShouldEqual("150");
+
+
+                // Assert that there is no value for this response header
+                _.Header("connection").ShouldNotBeWritten();
+
+
+                // Only write one value for this header
+                _.Header("set-cookie").ShouldHaveOneNonNullValue();
+            });
+        }
+        // ENDSAMPLE
+
+
+    }
+}
