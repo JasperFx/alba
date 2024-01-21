328d036d49a4b346cd44603a257e44295f47774f
JT <Hawxy@users.noreply.github.com>
2024-01-21 16:27:01 +0800

    Swap build system to nuke

--- build.cmd
+++ build.cmd
@@ -1,2 +1,7 @@
-@echo off
-dotnet run --project build/build.csproj -c Release -- %*
+:; set -eo pipefail
+:; SCRIPT_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
+:; ${SCRIPT_DIR}/build.sh "$@"
+:; exit $?
+
+@ECHO OFF
+powershell -ExecutionPolicy ByPass -NoProfile -File "%~dp0build.ps1" %*
--- build/build.cs
+++ build/build.cs
@@ -1,212 +1,104 @@
 using System;
-using System.IO;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using static Bullseye.Targets;
-using static SimpleExec.Command;
+using System.Linq;
+using Nuke.Common;
+using Nuke.Common.CI;
+using Nuke.Common.CI.GitHubActions;
+using Nuke.Common.Execution;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Utilities.Collections;
+using static Nuke.Common.EnvironmentInfo;
+using static Nuke.Common.IO.FileSystemTasks;
+using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
-namespace build
+
+[GitHubActions(
+    "Build & Test",
+    GitHubActionsImage.UbuntuLatest,
+    OnPushBranches = new []{ "master" },
+    OnPullRequestBranches = new []{ "master" },
+    InvokedTargets = new[] { nameof(Test) })]
+[GitHubActions(
+    "Manual Nuget Push",
+    GitHubActionsImage.UbuntuLatest,
+    On = new[] { GitHubActionsTrigger.WorkflowDispatch },
+    InvokedTargets = new[] { nameof(NugetPush) },
+    ImportSecrets = new[] { nameof(NugetApiKey) })]
+class Build : NukeBuild
 {
-    internal class Build
-    {
-        private const string BUILD_VERSION = "6.1.0";
-        private const string GITHUB_REPO = "https://github.com/jasperfx/alba.git";
+    /// Support plugins are available for:
+    ///   - JetBrains ReSharper        https://nuke.build/resharper
+    ///   - JetBrains Rider            https://nuke.build/rider
+    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
+    ///   - Microsoft VSCode           https://nuke.build/vscode
+    
+    public static int Main () => Execute<Build>(x => x.Compile);
+    
+    [Solution(GenerateProjects = true)] readonly Solution Solution;
+    AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
 
-
-        private static Task Main(string[] args)
+    Target Clean => _ => _
+        .Before(Restore)
+        .Executes(() =>
         {
+            ArtifactsDirectory.CreateOrCleanDirectory();
+        });
+
+    Target Restore => _ => _
+        .Executes(() =>
+        {
+            DotNetRestore(s => s
+                .SetProjectFile(Solution));
+        });
+
+    Target Compile => _ => _
+        .DependsOn(Restore)
+        .Executes(() =>
+        {
+            DotNetBuild(s => s
+                .SetProjectFile(Solution)
+                .SetConfiguration("Release")
+                .EnableNoRestore());
+        });
+    
+    Target Test => _ => _
+        .DependsOn(Compile)
+        .Executes(() =>
+        {
+            DotNetTest(_ => _
+                .SetProjectFile(Solution.Alba_Testing));
             
-            Target("default", DependsOn("test"));
-            
-            Target("restore", () =>
-            {
-                Run("dotnet", "restore src/Alba.sln");
-            });
-           
-            Target("test", DependsOn("restore"),() =>
-            {
-                RunTests("Alba.Testing");
-                RunTests("NUnitSamples");
-            });
+            DotNetTest(_ => _
+                .SetProjectFile(Solution.NUnitSamples));
+        });
 
-            Target("ci", DependsOn("default"));
-
-            Target("install", () =>
-                RunNpm("install"));
-
-            Target("compile",  DependsOn("restore"),() =>
-            {
-                Run("dotnet",
-                    $"build src/Alba.sln");
-            });
-            
-            Target("install-mdsnippets", IgnoreIfFailed(() =>
-                Run("dotnet", $"tool install -g MarkdownSnippets.Tool")
-            ));
-
-            Target("docs", DependsOn("install", "install-mdsnippets"), () => {
-                // Run docs site
-                RunNpm("run docs");
-            });
-
-            Target("docs-build", DependsOn("install", "install-mdsnippets"), () => {
-                // Run docs site
-                RunNpm("run docs-build");
-            });
-
-            Target("clear-inline-samples", () => {
-                var files = Directory.GetFiles("./docs", "*.md", SearchOption.AllDirectories);
-                var pattern = @"<!-- snippet:(.+)-->[\s\S]*?<!-- endSnippet -->";
-                var replacePattern = $"<!-- snippet:$1-->{Environment.NewLine}<!-- endSnippet -->";
-                foreach (var file in files)
-                {
-                    // Console.WriteLine(file);
-                    var content = File.ReadAllText(file);
-
-                    if (!content.Contains("<!-- snippet:")) {
-                        continue;
-                    }
-
-                    var updatedContent = Regex.Replace(content, pattern, replacePattern);
-                    File.WriteAllText(file, updatedContent);
-                }
-            });
-
-            Target("publish-docs", DependsOn("docs-build"), () =>
-            {
-                PublishDocs(GITHUB_REPO);
-            });
-
-            Target("publish-docs-gh-actions", DependsOn("docs-build"), () =>
-            {
-                var ghActor = GetEnvironmentVariable("GITHUB_ACTOR");
-                var ghToken = GetEnvironmentVariable("GITHUB_TOKEN");
-                var ghRepo = GetEnvironmentVariable("GITHUB_REPOSITORY");
-                var repo = $"https://{ghActor}:{ghToken}@github.com/{ghRepo}.git";
-                PublishDocs(repo, true);
-            });
-
-            return RunTargetsAndExitAsync(args);
-            
-        }
-
-        private static void PublishDocs(string repo, bool isGHActionContext=false)
+    Target NugetPack => _ => _
+        .DependsOn(Compile)
+        .Executes(() =>
         {
-            var docTargetDir = "doc-target";
-            var branchName = "gh-pages";
-            Run("git", $"clone -b {branchName} {repo} {InitializeDirectory(docTargetDir)}");
+            DotNetPack(_ => _
+                .SetProject(Solution.Alba)
+                .SetConfiguration("Release")
+                .EnableContinuousIntegrationBuild()
+                .SetOutputDirectory(ArtifactsDirectory));
+        });
 
-            if (isGHActionContext) {
-                Run("git", "config user.email \"action@github.com\"", docTargetDir);
-                Run("git", "config user.name \"GitHub Action\"", docTargetDir);
-            } else {
-                // if you are not using git --global config, un-comment the block below, update and use it
-                // Run("git", "config user.email user_email", docTargetDir);
-                // Run("git", "config user.name user_name", docTargetDir);
-            }
+    [Parameter("Nuget Api Key")] [Secret] readonly string NugetApiKey;
 
-            // Clean off all the files in the doc-target directory
-            foreach (var file in Directory.EnumerateFiles(docTargetDir))
-            {
-                if (Path.GetFileName(file) == ".nojekyll") continue;
-                
-                File.Delete(file);
-            }
-
-            var buildDir = Path.Combine(Environment.CurrentDirectory, "docs", ".vitepress", "dist");
-
-            CopyFilesRecursively(buildDir, docTargetDir);
-
-            Run("git", "add --all", docTargetDir);
-            Run("git", $"commit -a -m \"Documentation Update for {BUILD_VERSION}\" --allow-empty", docTargetDir);
-            Run("git", $"push origin {branchName}", docTargetDir);
-        }
-
-
-        private static void CopyFilesRecursively(string sourcePath, string targetPath)
+    Target NugetPush => _ => _
+        .DependsOn(NugetPack)
+        .Requires(() => !string.IsNullOrEmpty(NugetApiKey))
+        .Executes(() =>
         {
-            //Now Create all of the directories
-            foreach (string dirPath in Directory.GetDirectories(sourcePath, "*", SearchOption.AllDirectories))
-            {
-                Directory.CreateDirectory(dirPath.Replace(sourcePath, targetPath));
-            }
-
-            //Copy all the files & Replaces any files with the same name
-            foreach (string newPath in Directory.GetFiles(sourcePath, "*.*",SearchOption.AllDirectories))
-            {
-                File.Copy(newPath, newPath.Replace(sourcePath, targetPath), true);
-            }
-        }
-
-        private static string InitializeDirectory(string path)
-        {
-            EnsureDirectoriesDeleted(path);
-            Directory.CreateDirectory(path);
-            return path;
-        }
-
-        private static void EnsureDirectoriesDeleted(params string[] paths)
-        {
-            foreach (var path in paths)
-            {
-                if (Directory.Exists(path))
-                {
-                    var dir = new DirectoryInfo(path);
-                    DeleteDirectory(dir);
-                }
-            }
-        }
-
-        private static void DeleteDirectory(DirectoryInfo baseDir)
-        {
-            baseDir.Attributes = FileAttributes.Normal;
-            foreach (var childDir in baseDir.GetDirectories())
-                DeleteDirectory(childDir);
-
-            foreach (var file in baseDir.GetFiles())
-                file.IsReadOnly = false;
-
-            baseDir.Delete(true);
-        }
-
-        private static void RunNpm(string args) =>
-            Run("npm", args);
-
-        private static void RunTests(string projectName, string directoryName = "src")
-        {
-            Run("dotnet", $"test --no-restore {directoryName}/{projectName}/{projectName}.csproj");
-        }
-        
-        private static string GetEnvironmentVariable(string variableName)
-        {
-            var val = Environment.GetEnvironmentVariable(variableName);
-
-            // Azure devops converts environment variable to upper case and dot to underscore
-            // https://docs.microsoft.com/en-us/azure/devops/pipelines/process/variables?view=azure-devops&tabs=yaml%2Cbatch
-            // Attempt to fetch variable by updating it
-            if (string.IsNullOrEmpty(val))
-            {
-                val = Environment.GetEnvironmentVariable(variableName.ToUpper().Replace(".", "_"));
-            }
-
-            Console.WriteLine(val);
-
-            return val;
-        }
-
-        private static Action IgnoreIfFailed(Action action)
-        {
-            return () =>
-            {
-                try
-                {
-                    action();
-                }
-                catch (Exception exception)
-                {
-                    Console.WriteLine(exception.Message);
-                }
-            };
-        }
-    }
+            DotNetNuGetPush(_ => _
+                .SetSource("https://api.nuget.org/v3/index.json")
+                .SetTargetPath(ArtifactsDirectory / "*.nupkg")
+                .EnableSkipDuplicate()
+                .EnableNoSymbols()
+                .SetApiKey(NugetApiKey));
+        });
+    
 }
