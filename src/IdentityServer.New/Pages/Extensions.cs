372bbde8093a5e510bb4e4d5ac10580c1e1a2555
Simon Cropp <simon.cropp@gmail.com>
2025-04-27 13:06:06 +1000

    Use well known header properties (#191)

--- src/IdentityServer.New/Pages/Extensions.cs
+++ src/IdentityServer.New/Pages/Extensions.cs
@@ -36,7 +36,7 @@ namespace IdentityServer.New.Pages
         public static IActionResult LoadingPage(this PageModel page, string redirectUri)
         {
             page.HttpContext.Response.StatusCode = 200;
-            page.HttpContext.Response.Headers["Location"] = "";
+            page.HttpContext.Response.Headers.Location = "";
 
             return page.RedirectToPage("/Redirect/Index", new { RedirectUri = redirectUri });
         }
